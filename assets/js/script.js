2633f047f33a62e791644cfd8110bcdfa9990d15
Meghanarajvinakota <meghana.penugonda@gmail.com>
2024-03-28 10:11:28 +0000

    Add three more circles to increse level to hard

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+var numSquares = 9;
+var colors = generateRandomColors(numSquares);
+var squares = document.querySelectorAll(".square");
+var pickedColor = randomColorG();
+var colorDisplay = document.querySelector("#colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyButton");
+var hardBtn = document.querySelector("#hardButton");
+function randomColor(){
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	// pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r +", " + g +", " + b +")";
+}
